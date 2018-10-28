8e41aed2ca70c0edc0cee17982d8b3e447a8ffb0
Tommaso Sardelli <lacapannadelloziotom@gmail.com>
2018-10-28 22:29:04 +0100

    Generate teacher resource

---
+++ actions/teachers_test.go
@@ -0,0 +1,29 @@
+package actions
+
+func (as *ActionSuite) Test_TeachersResource_List() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_TeachersResource_Show() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_TeachersResource_New() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_TeachersResource_Create() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_TeachersResource_Edit() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_TeachersResource_Update() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_TeachersResource_Destroy() {
+	as.Fail("Not Implemented!")
+}
---
+++ models/teacher.go
@@ -0,0 +1,50 @@
+package models
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/gobuffalo/pop"
+	"github.com/gobuffalo/uuid"
+	"github.com/gobuffalo/validate"
+)
+
+type Teacher struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+	UserID    uuid.UUID `db:"user_id"`
+}
+
+// String is not required by pop and may be deleted
+func (t Teacher) String() string {
+	jt, _ := json.Marshal(t)
+	return string(jt)
+}
+
+// Teachers is not required by pop and may be deleted
+type Teachers []Teacher
+
+// String is not required by pop and may be deleted
+func (t Teachers) String() string {
+	jt, _ := json.Marshal(t)
+	return string(jt)
+}
+
+// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
+// This method is not required and may be deleted.
+func (t *Teacher) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
+// This method is not required and may be deleted.
+func (t *Teacher) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
+// This method is not required and may be deleted.
+func (t *Teacher) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
