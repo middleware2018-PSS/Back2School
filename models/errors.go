495839ceaf0b9ccf735af17f609a24a41dfa9972
Tommaso Sardelli <lacapannadelloziotom@gmail.com>
2018-11-23 19:08:23 +0100

    Add missing date of birth validation message

--- models/errors.go
+++ models/errors.go
@@ -1,9 +1,10 @@
 package models
 
 const (
-	mailValidationMsg     = "Mail field is empty or not in the right format"
-	nameValidationMsg     = "Name field cannot be empty"
-	surnameValidationMsg  = "Surname field cannot be empty"
-	passwordValidationMsg = "Password field cannot be empty"
-	roleValidationMsg     = "Role field cannot be empty"
+	mailValidationMsg        = "Mail field is empty or not in the right format"
+	nameValidationMsg        = "Name field cannot be empty"
+	surnameValidationMsg     = "Surname field cannot be empty"
+	passwordValidationMsg    = "Password field cannot be empty"
+	roleValidationMsg        = "Role field cannot be empty"
+	dateOfBirthValidationMsg = "Date of Birth field cannot be empty"
 )
