dfa3f47796b5a220e91bef1fe7d2206df3bab781
Tommaso Sardelli <lacapannadelloziotom@gmail.com>
2018-11-24 12:41:21 +0100

    Move env vars from create_users.sh to .env

--- create_users.sh
+++ create_users.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
 
-
-APIURL="http://127.0.0.1:3000/api/v1"
-JWT_TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6ImFmODJhYWI5LWNjYjctNDBiYS1hZTI1LTllNzBlMDhhZDc4MyIsImV4cCI6MTU0MjA2NDI0OCwicm9sZSI6ImFkbWluIiwicm9sZV9pZCI6ImFmODJhYWI5LWNjYjctNDBiYS1hZTI1LTllNzBlMDhhZDc4MyJ9.bKnUf3AZrjEgUY7GQKUN8jqmBDWfr9N7yNfkixYVOJI"
-
 for p in payloads/admin_*; do http --auth-type=jwt --auth="$JWT_TOKEN" $APIURL/admins < $p; done
 for p in payloads/parent_*; do http --auth-type=jwt --auth="$JWT_TOKEN" $APIURL/parents < $p; done
 for p in payloads/teacher_*; do http --auth-type=jwt --auth="$JWT_TOKEN" $APIURL/teachers < $p; done
+#for p in payloads/student_*; do http --auth-type=jwt --auth="$JWT_TOKEN" $APIURL/students < $p; done
